204d5a63e0853f1e8357bfeb6f9528757eada3b9
Santosh <poptanisantosh@gmail.com>
2024-04-22 12:22:45 +0530

    pass 2nd stage added printwriter to send data to clinet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,6 +7,7 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
+      final String pong = "+PONG\r\n";
 
     //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
@@ -18,6 +20,8 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            out.printf(pong);
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
