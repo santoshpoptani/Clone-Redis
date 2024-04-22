1a128839cc479a3bde1f85b0befb9f9243aca86a
Santosh <poptanisantosh@gmail.com>
2024-04-22 12:38:57 +0530

    Modified the code to listen to multiple commands in single connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,8 +19,25 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.printf(pong);
+          InputStream in = clientSocket.getInputStream();
+          InputStreamReader reader = new InputStreamReader(in);
+          BufferedReader bufferedReader = new BufferedReader(reader);
+          // Get the output stream from the client socket and write data to it using
+          // a BufferedWriter
+          OutputStreamWriter writer =
+                  new OutputStreamWriter(clientSocket.getOutputStream());
+          BufferedWriter bufferedWriter = new BufferedWriter(writer);
+          String command;
+          while ((command = bufferedReader.readLine()) != null) {
+            // If the command is PING, write +PONG back to the client
+            if (command.equalsIgnoreCase("ping")) {
+              // Read the data from the client and write it back to the client
+              bufferedWriter.write("+PONG\r\n");
+            }
+            // Flush the buffered writer
+            bufferedWriter.flush();
+          }
+
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
